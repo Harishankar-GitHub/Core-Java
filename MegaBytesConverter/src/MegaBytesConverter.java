3ae633aed91818c371bb63174ab84a4a1f2174d5
HARISHANKAR BHAT R <rhsb1997@gmail.com>
2020-05-03 13:35:51 +0530

    MegaBytesConverter

---
+++ MegaBytesConverter/src/MegaBytesConverter.java
@@ -0,0 +1,18 @@
+public class MegaBytesConverter {
+
+    public static void printMegaBytesAndKiloBytes (int kiloBytes)
+    {
+        if (kiloBytes < 0)
+        {
+            System.out.println("Invalid Value");
+        }
+        else
+        {
+            int megabyte = kiloBytes / 1024;
+            int kilobyte = kiloBytes % 1024;
+
+            System.out.println(kiloBytes + " KB = " + megabyte + " MB and " + kilobyte + " KB");
+
+        }
+    }
+}
